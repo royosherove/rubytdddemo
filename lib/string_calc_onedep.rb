a266cbb99bd4d5671a9d64fd8e0b8f3d6a07252e
Roy Osherove <roy@osherove.com>
2013-09-08 15:41:17 +0200

    progress

--- lib/string_calc_onedep.rb
+++ lib/string_calc_onedep.rb
@@ -1,10 +1,24 @@
-class StringCalculator
+class StringCalculatorOneDep
   attr_accessor :last_sum
+  def initialize(thelogger,thewebserice)
+    @last_sum = 0
+    @logger = thelogger
+    @webservice = thewebserice
+  end
 
   def add(numbers)
     raise "only positives please!" if numbers.include?("-")
     @last_sum = calculate(numbers)
+    notify_logger
     @last_sum
+  end
+
+  def notify_logger
+    begin
+      @logger.write("got #{@last_sum}")
+    rescue
+      @webservice.notify("logger threw up")
+    end
   end
 
   def calculate(numbers)
