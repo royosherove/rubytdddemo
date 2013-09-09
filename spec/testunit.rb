faa2ddfd8dff8c0de7a6a217cfac8462d8fe2c26
Roy Osherove <roy@osherove.com>
2013-09-09 06:52:05 +0200

    progress

--- spec/testunit.rb
+++ spec/testunit.rb
@@ -4,6 +4,8 @@ require '../lib/string_calc'
 class TestTheCalc< Test::Unit::TestCase
   def test_add_emptystring_returnsdefaultvalue
     
+    result = StringCalculator.new.add("")
+    assert_equal 0,result
 
   end
 end
