8974d9a49c54dc7bb6f477ee8c886fd99e4299c6
Roy Osherove <roy@osherove.com>
2013-09-10 10:00:31 +0200

    progress

--- spec/string_calc_spec.rb
+++ spec/string_calc_spec.rb
@@ -6,37 +6,30 @@ require_relative '../lib/string_calc.rb'
 describe StringCalculator do
 
   describe "Adding" do
-    def make_calc
-      StringCalculator.new
-    end
-
+    let(:calc){StringCalculator.new}
 
     context "negative numbers" do
 
-          it "does something really important"
-
           it "throws for single number" do
-            expect { make_calc.add("-1") }.to raise_error
+            expect { calc.add("-1") }.to raise_error
           end
 
           it "throws for single number within other numbers" do
-            expect { make_calc.add("-1,2") }.to raise_error
+            expect { calc.add("-1,2") }.to raise_error
           end
 
           it "throws for several negatives" do
-            lambda { make_calc.add("1,-1,-2") }.should raise_error
+            lambda { calc.add("1,-1,-2") }.should raise_error
           end
     end
 
     context "a single number" do
 
           it "returns the same number" do
-            @calc = make_calc
             expect_that "2",2
           end
 
           it "returns the same number2" do
-            @calc = make_calc
             expect_that "1",1
           end
 
@@ -45,11 +38,9 @@ describe StringCalculator do
 
     context "multiple numbers" do
       it "handles two numbers" do
-        @calc = make_calc
         expect_that "1,2",3
       end
       it "handles three numbers" do
-        @calc = make_calc
         expect_that "1,2,3",6
       end
 
@@ -57,26 +48,24 @@ describe StringCalculator do
 
     describe "remembers last sum" do
       it "returns the last sum form last operation made" do
-        @calc = make_calc
-        @calc.add "1"
-        @calc.last_sum.should == 1
+        calc.add "1"
+        calc.last_sum.should == 1
       end
 
       it "returns zero if not operations were made" do
-        make_calc.last_sum.should == 0
+        calc.last_sum.should == 0
       end
     end
 
     def expect_that(input,expected)
-      @calc.add(input).should == expected
+      calc.add(input).should == expected
     end
 
 
     context "an empty value" do
 
       it "returns the default value" do
-        @calc = make_calc
-        @calc.add("").should == 0
+        calc.add("").should == 0
       end
     end
   end
