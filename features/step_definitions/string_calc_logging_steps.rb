2bc29c0a29bfef8150413cd59768eb367561b416
Roy Osherove <roy@osherove.com>
2013-09-12 11:45:40 +0200

    progress

--- features/step_definitions/string_calc_logging_steps.rb
+++ features/step_definitions/string_calc_logging_steps.rb
@@ -3,15 +3,26 @@ require_relative '../../lib/slow_logger.rb'
 require_relative '../../lib/string_calc.rb'
 require 'bogus'
 
+class FakeWebService
+  def write(something)
+
+  end
+end
+class FakeLogger
+  attr_accessor :numbers
+  def write(something)
+    @numbers = something
+  end
+end
 Given(/^a string calculator$/) do
 end
 
 Given(/^a slow logger instance$/) do
-  @logger = fake(:slow_logger)
-  @calc = StringCalculator.new(@logger,fake(:web_service))
 end
 
 When(/^the string calculator add method is called with input  "(\d+),(\d+)"$/) do |arg1, arg2|
+  @logger = FakeLogger.new
+  @calc = StringCalculator.new(@logger,FakeWebService.new)
   @calc.add("#{arg1},#{arg2}")
 end
 
--- features/string_calc_logging.feature
+++ features/string_calc_logging.feature
@@ -4,6 +4,7 @@ Feature: String Calculator Logging
     we want to have logging enabled for the string calculator
     we will only use HAND WRITTEN mocks (no framework)
 
+	Background: A String Calculator has a Logger
     Scenario: Adding a number
         When the string calculator add method is called with input  "1,2"
         Then slow logger should be notified with "got 3"
